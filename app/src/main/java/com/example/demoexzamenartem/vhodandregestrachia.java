cf5c96500b63f656c63e04ed8e76650eba600732
Max Korobkov <goosekms1@gmail.com>
2023-05-31 17:59:13 +0300

    day1.4

--- app/src/main/java/com/example/demoexzamenartem/vhodandregestrachia.java
+++ app/src/main/java/com/example/demoexzamenartem/vhodandregestrachia.java
@@ -6,22 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class onboard3 extends AppCompatActivity {
+public class vhodandregestrachia extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_onboard3);
-        getSupportActionBar().hide();
+        setContentView(R.layout.activity_vhodandregestrachia);
     }
     public void run(View run) {
-        Intent intent = new Intent(onboard3.this, vhodandregestrachia.class);
+        Intent intent = new Intent(vhodandregestrachia.this, vhodandregestrachia.class);
         startActivity(intent);
     }
     public void onClickLayout(View run) {
-        Intent intent = new Intent(onboard3.this, vhodandregestrachia.class);
+        Intent intent = new Intent(vhodandregestrachia.this, vhodandregestrachia.class);
         startActivity(intent);
     }
 }
-
-
