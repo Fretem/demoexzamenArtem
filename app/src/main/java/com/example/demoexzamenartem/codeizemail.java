393dccbdcdc990315eaef3c6d952fbbdbc28003c
Max Korobkov <goosekms1@gmail.com>
2023-06-01 14:21:17 +0300

    day2.1

--- app/src/main/java/com/example/demoexzamenartem/codeizemail.java
+++ app/src/main/java/com/example/demoexzamenartem/codeizemail.java
@@ -6,20 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class vhodandregestrachia extends AppCompatActivity {
+public class codeizemail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vhodandregestrachia);
+        setContentView(R.layout.activity_codeizemail);
         getSupportActionBar().hide();
     }
     public void run(View run) {
-        Intent intent = new Intent(vhodandregestrachia.this, codeizemail.class);
+        Intent intent = new Intent(codeizemail.this, vhodandregestrachia.class);
         startActivity(intent);
     }
     public void onClickLayout(View run) {
-        Intent intent = new Intent(vhodandregestrachia.this, codeizemail.class);
+        Intent intent = new Intent(codeizemail.this, codeizemail.class);
         startActivity(intent);
     }
+
 }
