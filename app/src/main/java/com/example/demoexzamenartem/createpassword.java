f9c80dff276e2ac8041e3cb481bdd00893ebb657
Fretem <artem.anisimov2004@gmail.com>
2023-06-28 21:38:55 +0300

    exzamenfinal

--- app/src/main/java/com/example/demoexzamenartem/createpassword.java
+++ app/src/main/java/com/example/demoexzamenartem/createpassword.java
@@ -2,7 +2,9 @@ package com.example.demoexzamenartem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class createpassword extends AppCompatActivity {
 
@@ -11,5 +13,14 @@ public class createpassword extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createpassword);
         getSupportActionBar().hide();
+
+    }
+    public void run(View run) {
+        Intent intent = new Intent(createpassword.this, exzamen.class);
+        startActivity(intent);
+    }
+    public void onClickLayout(View run) {
+        Intent intent = new Intent(createpassword.this, exzamen.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/demoexzamenartem/exzamen.java
@@ -0,0 +1,18 @@
+package com.example.demoexzamenartem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class exzamen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exzamen);
+        getSupportActionBar().hide();
+    }
+
+}
